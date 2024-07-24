b793fb3a74b50f3f135aaa22fb653a35ee278b06
22f3001517 <22f3001517@ds.study.iitm.ac.in>
2024-07-24 23:25:43 +0530

    Update data_cleanup_01.sh

--- data_cleanup_01.sh
+++ data_cleanup_01.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 jq 'map(del(."Unnamed: 0")' fetched_data_all_haryana.json | jq 'map(.AC |= sub("^[0-9]+ "; ""))' | jq 'map(.CANDIDATE |= sub("^[0-9]+ "; ""))' > cleaned_all_haryana.json
+jq 'unique' cleaned_all_haryana.json > temp_cleaned_all_haryana.json
+mv temp_cleaned_all_haryana.json cleaned_all_haryana.json
